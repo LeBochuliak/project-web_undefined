43d7ff336e068d871a588cad9246b7192b512744
Di <diana8burkut@gmail.com>
2025-04-03 23:55:08 +0200

    1. додала базові елементи до розмітки і класи 2. додала трішки стилізації(перша підсекція моєї секції) 3. імпортувала бібліотеки

---
+++ src/js/about-me.js
@@ -0,0 +1,19 @@
+// Бібліотеки
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+import Swiper from 'swiper';
+import 'swiper/css';
+// !Бібліотеки 
+
+
+const container = document.querySelector('.accordion-container');
+const accordion = new Accordion(container, {
+    showMultiple: true,
+    onOpen: (currEl) => console.log('Open!', currEl)
+});
+ 
+const buttons = accContainer.querySelectorAll('.close-accordion-btn');
+[...buttons].map((button, idx) => {
+    button.addEventListener('click', () => accordion.close(idx));
+});
