8bfeba85d46acb3962a924072b787b3f49f26f08
Di <diana8burkut@gmail.com>
2025-04-04 20:54:22 +0200

    домучила акордеон на мобільну версію(js, стилі,  переробила верстку)

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -4,16 +4,30 @@ import "accordion-js/dist/accordion.min.css";
 
 import Swiper from 'swiper';
 import 'swiper/css';
-// !Бібліотеки 
+// !Бібліотеки
+document.addEventListener("DOMContentLoaded", () => {
+    const accEls = document.querySelectorAll(".accordion-el");
+    [...accEls].forEach((accEl) => {
+        if (accEl.classList.contains("is-active")) {
+            const icon = accEl.querySelector(".arrow-top");
+            if (icon) {
+                icon.style.transform = "rotate(0deg)";
+            }
+        }
+    });
+})
 
 
-const container = document.querySelector('.accordion-container');
-const accordion = new Accordion(container, {
-    showMultiple: true,
-    onOpen: (currEl) => console.log('Open!', currEl)
-});
- 
-const buttons = accContainer.querySelectorAll('.close-accordion-btn');
-[...buttons].map((button, idx) => {
-    button.addEventListener('click', () => accordion.close(idx));
+const accordion = new Accordion(".accordion-container", {
+    openOnInit: [0],
+    duration: 400,
+    showMultiple: false,
+    onOpen: (currElement) => {
+        const icon = currElement.querySelector(".arrow-top");
+        if (icon) icon.style.transform = "rotate(0deg)";
+    },
+    onClose: (currElement) => {
+        const icon = currElement.querySelector(".arrow-top");
+        if (icon) icon.style.transform = "rotate(180deg)";
+    },
 });
