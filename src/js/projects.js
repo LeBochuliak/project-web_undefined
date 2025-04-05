9d1197681cb14b5268ca09e3f46b2f6359a65043
AlinaMyrosh <154742742+AlinaMyrosh@users.noreply.github.com>
2025-04-05 21:05:19 +0200

    feature/section-projects-js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,28 +1,55 @@
-// import Swiper from 'swiper';
-// import { Navigation } from 'swiper/modules';
-// import 'swiper/css';
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel, A11y } from 'swiper/modules';
+import 'swiper/css';
 
-// const swiper = new Swiper('.swiper', {
-//   // configure Swiper to use modules
-//   modules: [Navigation],
-//   direction: 'horizontal',
-//   slidesPerView: 1,
-//   keyboard: {
-//     enabled: true,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+const prevButton = document.querySelector('.projects-prev-btn');
+const nextButton = document.querySelector('.projects-next-btn');
 
-// const sectionReview = document.querySelector('.reviews');
-// sectionReview.addEventListener('keydown', event => {
-//   // console.log('e', event)
-//   event.preventDefault();
-//   if (event.code === 'ArrowRight') {
-//     swiper.slideNext(300);
-//   } else if (event.code === 'ArrowLeft') {
-//     swiper.slidePrev(300);
-//   }
-// });
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Keyboard, Mousewheel, A11y],
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 40,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  mousewheel: {
+    forceToAxis: true,
+  },
+  navigation: {
+    nextEl: '.projects-next-btn',
+    prevEl: '.projects-prev-btn',
+  },
+  a11y: {
+    enabled: true,
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide',
+  },
+  on: {
+    init: function () {
+      updateButtonState(this);
+    },
+    slideChange: function () {
+      updateButtonState(this);
+    },
+  },
+});
+
+function updateButtonState(swiper) {
+  if (swiper.isBeginning) {
+    prevButton.classList.add('projects-btn-disabled');
+    prevButton.disabled = true;
+  } else {
+    prevButton.classList.remove('projects-btn-disabled');
+    prevButton.disabled = false;
+  }
+
+  if (swiper.isEnd) {
+    nextButton.classList.add('projects-btn-disabled');
+    nextButton.disabled = true;
+  } else {
+    nextButton.classList.remove('projects-btn-disabled');
+    nextButton.disabled = false;
+  }
+}
