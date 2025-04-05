846496ab542ef467011b1c126c0694f2deeecdfe
Di <diana8burkut@gmail.com>
2025-04-06 00:29:14 +0200

    Реалізувала свайпер додала розмітку підсекції додала реалізацію бібліотеки додала стилі додала респонсив

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,11 +3,12 @@ import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
 
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper/css/pagination';
 // !Бібліотеки
+//  Swiper.use([Navigation]);
 
                 // Акордеон
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,54 +21,85 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
-})
 
-const accordion = new Accordion(".accordion-container", {
-    openOnInit: [0],
-    duration: 400,
-    showMultiple: false,
-    onOpen: (currElement) => {
-        const icon = currElement.querySelector(".arrow-top");
-        if (icon) icon.style.transform = "rotate(0deg)";
-    },
-    onClose: (currElement) => {
-        const icon = currElement.querySelector(".arrow-top");
-        if (icon) icon.style.transform = "rotate(180deg)";
-    },
-});
-                // ! Акордеон
+    const accordion = new Accordion(".accordion-container", {
+        openOnInit: [0],
+        duration: 400,
+        showMultiple: false,
 
-                // Свайпер
-const swiper = new Swiper('.swiper', {
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 0
+        onOpen: (currElement) => {
+            const icon = currElement.querySelector(".arrow-top");
+            if (icon) icon.style.transform = "rotate(0deg)";
         },
-        // when window width is >= 480px
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 0
+        onClose: (currElement) => {
+            const icon = currElement.querySelector(".arrow-top");
+            if (icon) icon.style.transform = "rotate(180deg)";
         },
-        // when window width is >= 640px
-        1440: {
-            slidesPerView: 6,
-            spaceBetween: 0
+    });
+    // ! Акордеон
+
+    // Свайпер
+
+    //     const swiper = new Swiper('.swiper', {
+    //         slidesPerGroup: 1,   
+    //         spaceBetween: 0,
+    //         centeredSlides: false,   
+    //         // uniqueNavElements: true,
+    //         speed: 400,
+    //   direction: 'horizontal',
+    //   loop: true,
+    //         loopAddBlankSlides: true,	
+    //     slideToClickedSlide: false,  
+    //   navigation: {
+    //     nextEl: '.swiper-button-next',
+    //   },
+    //   breakpoints: {
+    //         320: {
+    //         slidesPerGroup:  1,
+    //       slidesPerView: 2,
+    //     },
+    //         768: {
+    //             slidesPerGroup:  1,
+    //       slidesPerView: 3,
+    //     },
+    //         1440: {
+    //             slidesPerGroup:  1,
+    //       slidesPerView: 3,
+    //     }
+    //     },
+    
+    // });
+
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        loopAdditionalSlides: 2,
+        slidesPerGroup:  1,
+        modules: [Navigation],
+            //         spaceBetween: 0,
+
+        slidesPerView: 2,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            
+        },
+           breakpoints: {
+               375: {
+            slidesPerGroup:  1,
+          slidesPerView: 2,
+        },
+            768: {
+                slidesPerGroup:  1,
+          slidesPerView: 3,
+        },
+            1440: {
+                slidesPerGroup:  1,
+          slidesPerView: 6,
         }
-    },
-    speed: 400,
-        // spaceBetween: 100,
-     direction: 'horizontal',
-    loop: true,
-        // pagination: {
-        // el: '.swiper-pagination',
-        // },
-     navigation: {
-        nextEl: '.swiper-button-next',
-        // prevEl: '.swiper-button-prev',
         },
-});
-console.log(swiper.params);
+    });
 
-                // ! Свайпер
+
+
+});
+
