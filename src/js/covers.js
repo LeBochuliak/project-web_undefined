adb50e3d6b39a6e6196eb6be6e5029e07a3d30de
LeBochuliak <elenadolinskaya03@gmail.com>
2025-04-06 14:25:21 +0200

    Add images for retina-mobile. Add js for section covers

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,6 +1,33 @@
-const originalList = document.querySelector('.covers-line');
-for (let i = 0; i < 4; i++) {
-  const copy = originalList.cloneNode(true);
-  copy.classList.add('cloned-item');
-  document.querySelector('.cloned-line').appendChild(copy);
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const originalLists = document.querySelectorAll('.covers-line');
+  originalLists.forEach(list => {
+    let copy = list.cloneNode(true);
+    copy.classList.add('cloned-line');
+    const items = copy.querySelectorAll('li');
+    copy.prepend(items[items.length - 1].cloneNode(true));
+    copy.prepend(items[items.length - 2].cloneNode(true));
+    document.querySelector('.cloned-lines').appendChild(copy);
+  });
+
+  const coversSection = document.querySelector('.covers-section');
+  const coversContent = document.querySelector('.covers-content');
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          coversContent.style.animationPlayState = 'running';
+        } else {
+          coversContent.style.animationPlayState = 'paused';
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+  );
+
+  if (coversSection) {
+    observer.observe(coversSection);
+  }
+});
