744581644447c4232d9ece70512b456dbcdfa3d9
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-04-07 20:28:02 +0200

    personalization-faq-stiles

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,15 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion(['.accordion-container2'], {
-  duration: 300,
+new Accordion(['.accordion-container-faq'], {
+  duration: 100,
   showMultiple: false,
   onOpen: currElement => {
-    const icon = currElement.querySelector('.arrow-top');
+    const icon = currElement.querySelector('.arrow-top-faq');
     if (icon) icon.style.transform = 'rotate(0deg)';
   },
   onClose: currElement => {
-    const icon = currElement.querySelector('.arrow-top');
+    const icon = currElement.querySelector('.arrow-top-faq');
     if (icon) icon.style.transform = 'rotate(180deg)';
   },
 });
