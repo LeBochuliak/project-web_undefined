9385a7d801086a629814331b606247dbe9ae2fee
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-04-07 16:31:32 +0200

    new-commit-faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion(['.accordion-container2', '.accordion-container3'], {
+new Accordion(['.accordion-container2'], {
   duration: 300,
   showMultiple: false,
   onOpen: currElement => {
